f622c35c1719e6151124512ee55204162592a6f1
github username <rjmascolo@gmail.com>
2018-03-09 08:51:17 -0500

    removed chrome devtools  extension

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
